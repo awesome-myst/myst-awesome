0d7a93e512562fe5c394afb12ef94ea732317952
Matt McCormick <matt@fideus.io>
2025-07-28 20:21:44 -0400

    docs: add @awesome-myst/ org to packages

--- packages/myst-awesome-theme/src/content.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineCollection, z } from "astro:content";
-
-// Simple content config for testing the Web Awesome asset copying
-// TODO: Restore full MyST integration with proper schema imports
-
-export const collections = {};
